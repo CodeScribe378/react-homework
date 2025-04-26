a507c4d626cf2a9151d15b2c556c52f4dcd0d7b9
CodeScribe378 <kristinahor91@gmail.com>
2025-04-26 14:02:57 +0200

    homework/lesson1/exercise1 is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 
 import './App.css'
+const coursesTitleArray: string[]=[
+  'JavaScript Complex',
+  'Java Complex',
+  'Python Complex',
+  'QA Complex',
+  'FullStack',
+  'Frontend',
+];
+
 
 function App() {
   return (
     <>
-
+      <ul>
+      {
+        coursesTitleArray.map((value, index)=>(
+          <li key={index}>{value}</li>)
+        )
+      }
+      </ul>
     </>
   )
 }
