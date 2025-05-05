58392407eb6bbd231b836ae9784274a9e4754572
CodeScribe378 <kristinahor91@gmail.com>
2025-05-05 23:32:38 +0200

    homework/lesson3/exercise1.1 is finished

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,25 @@
 
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {BrowserRouter, Route} from "react-router-dom";
-import ComponentsUsers from "./pages/ComponentsUsers.tsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import ComponentsPosts from "./pages/ComponentsPosts.tsx";
 import ComponentsComments from "./pages/ComponentsComments.tsx";
 import ComponentsProducts from "./pages/ComponentsProducts.tsx";
-// import App from './App.tsx'
+import MainLayout from "./MainLayout/MainLayout.tsx";
+import ComponentsUsers from "./pages/ComponentsUsers.tsx";
+
 
 createRoot(document.getElementById('root')!)
-.render (<BrowserRouter>
-    <Route path={'/'}>
+    .render(<BrowserRouter>
+
+        <Routes>
+    <Route path={'/'} element={<MainLayout/>}>
         <Route path={'users'} element={<ComponentsUsers/>}/>
         <Route path={'posts'} element={<ComponentsPosts/>}/>
         <Route path={'comments'} element={<ComponentsComments/>}/>
         <Route path={'products'} element={<ComponentsProducts/>}/>
 
     </Route>
+    </Routes>
+
 </BrowserRouter>)
